10f6885ed1935875e4d66cad0b1d30f683b52857
Mykolas Krupauskas <m.krupauskas@gmail.com>
2018-01-06 20:10:21 +0200

    use arguments to get the key

--- chapter2/vigenere.c
+++ chapter2/vigenere.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main(void)
+int main(int argc, char **argv)
 {
+    if (argc != 2)
+    {
+        printf("Incorrect argument count");
+        return 1;
+    }
+
+    char *key = argv[1];
 }
