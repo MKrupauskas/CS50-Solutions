d6236db5d00a88dbae9c243e86840a8c87ff4be4
Mykolas Krupauskas <m.krupauskas@gmail.com>
2018-01-10 21:56:07 +0200

    add vigenere solution

--- chapter2/vigenere.c
+++ chapter2/vigenere.c
@@ -1,16 +1,69 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main(int argc, char **argv)
+int string_length (char *string)
 {
+    int length;
+    for (length = 0; string[length] != '\0'; length++);
+    return length;
+}
+
+bool is_lower (char character)
+{
+    return (character >= 'a' && character <= 'z');
+}
+
+bool is_alpha (char character)
+{
+    return (character >= 'a' && character <= 'z') || (character >= 'A' && character <= 'Z');
+}
+
+void encrypt (char character, int key)
+{
+    // The 97 and 65 are for going to and from the alphabetical enumeration
+    // printf("%i", key);
+    int text_reference = (is_lower(character) ? 'a' : 'A');
+    printf("%c", (((character + (key  - (is_lower(key) ? 'a' : 'A'))) - text_reference) % 26) +
+        text_reference);
+}
+
+int main (int argc, char **argv)
+{
+    // There must be two arguments
     if (argc != 2)
     {
-        printf("Incorrect argument count");
+        printf("No key provided");
         return 1;
     }
 
+    // Get the key, turn it into an int
     char *key = argv[1];
+    for (int i = 0, l = string_length(key); i < l; i++)
+    {
+        if (!is_alpha(key[i]))
+        {
+            return 1;
+        }
+    }
+
+    // Get plaintext
     char *plaintext = get_string("plaintext: ");
 
-    printf(plaintext);
+
+    printf("ciphertext: ");
+    for (int i = 0, key_index = 0, l = string_length(plaintext); i < l; i++)
+    {
+        // Check if the char is in the alphabet
+        if (is_alpha(plaintext[i]))
+        {
+            encrypt(plaintext[i], key[key_index % string_length(key)]);
+            key_index++;
+        }
+        // If character is not alphabetic just print the character
+        else
+        {
+            printf("%c", plaintext[i]);
+        }
+    }
+    printf("\n");
 }
