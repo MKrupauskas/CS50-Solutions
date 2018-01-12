ca43564b28a60fe960e02963e070548181578884
Mykolas Krupauskas <m.krupauskas@gmail.com>
2018-01-12 21:39:28 +0200

    start working on crack solution

---
+++ chapter2/crack.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(int argc, char **argv)
+{
+    // There must be two arguments
+    if (argc != 2)
+    {
+        printf("No key provided");
+        return 1;
+    }
+}
