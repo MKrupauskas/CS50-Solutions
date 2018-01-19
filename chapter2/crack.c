a3077e30cef178c55c4d99926ad73fcd98001e7d
Mykolas Krupauskas <m.krupauskas@gmail.com>
2018-01-19 23:06:23 +0200

    loop through alphabet combinations

--- chapter2/crack.c
+++ chapter2/crack.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 #include <cs50.h>
-#define _XOPEN_SOURCE
-#include <unistd.h>
 
-int main(int argc, char **argv)
+int main (int argc, char **argv)
 {
     if (argc != 2)
     {
@@ -14,26 +12,78 @@ int main(int argc, char **argv)
     char salt[3];
     salt[0] = argv[1][0];
     salt[1] = argv[1][1];
-    salt[2] = '\0'
+    salt[2] = '\0';
 
-        char pass[4];
+    char pass[5];
+    pass[0] = '\0';
+    pass[1] = '\0';
+    pass[2] = '\0';
+    pass[3] = '\0';
+    pass[4] = '\0';
+
     char *characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 0; j < 52; j++)
-        {
-            pass[0] = characters[j];
-        }
 
-        if (i >= 1)
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     for (int j = 0; j < 52; j++)
+    //     {
+    //         pass[0] = characters[j];
+    //         if (i >= 1)
+    //         {
+    //             for (int k = 0; k < 52; k++)
+    //             {
+    //                 pass[1] = characters[k];
+
+    //                 if (i >= 2)
+    //                 {
+    //                     for (int l = 0; l < 52; l++)
+    //                     {
+    //                         pass[2] = characters[l];
+    //                         if (i >= 3)
+    //                         {
+    //                             for (int p = 0; p < 52; p++)
+    //                             {
+    //                                 pass[3] = characters[p];
+    //                                 printf("%s\n", pass);
+    //                             }
+    //                         }
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+
+    int abc(char *s, int size)
+    {
+        for (int i = 0; i < size; i++)
         {
-            for (int j = 0; j < 52; j++)
+            if (s[i] == '\0')
             {
-                pass[1] = characters[j];
+                s[i] = 'A';
+                break;
+            }
+            if (s[i] == 'Z')
+            {
+                s[i] = 'a';
+                break;
+            }
+            if (s[i] == 'z')
+            {
+                if (i == size - 1)
+                {
+                    return 0;
+                }
+                s[i] = 'A';
+            }
+            else
+            {
+                s[i]++;
+                break;
             }
         }
-
-        if (crypt(pass, salt) == hash)
-            printf("Your pass is &c", pass);
+        return 1;
     }
+    char *s = "aa";
+    printf(abc(s, 1));
 }
