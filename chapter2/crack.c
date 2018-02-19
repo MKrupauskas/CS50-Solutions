c8fd186c9513c0c958270025d02bb03a839a6e7b
Mykolas Krupauskas <m.krupauskas@gmail.com>
2018-02-19 22:31:55 +0200

    add best crack solution

--- chapter2/crack.c
+++ chapter2/crack.c
@@ -17,30 +17,32 @@ int main(int argc, char **argv)
     char *chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz", *hash;
     char password[5];
 
-    //testing for 1 chars words.
-    for (int i = 0; i < 52; i++)
-    {
-        password[0] = chars[i];
-        password[1] = '\0';
+    for (int i = 0; i < 4; i++)
+        password[i] = '\0';
 
-        hash = crypt(password, salt);
-        if (strcmp(argv[1], hash) == 0)
-        {
-            printf("%s %s %s\n", password, argv[1], hash);
-            return 0;
-        }
-    }
-
-    for (int j = 0; j < 52; j++)
+    for (int size = 0; size < 4; size++)
     {
-        for (int k = 0; k < 52; k++)
+        for (long long i = 0; i < pow(52, size + 1); i++)
         {
-            password[0] = chars[j];
-            password[1] = chars[k];
-            password[2] = '\0';
+            password[0] = chars[i % 52];
+
+            if (size >= 1)
+            {
+                password[1] = chars[(i / 52) % 52];
+            }
+            if (size >= 2)
+            {
+                password[2] = chars[(i / (int)pow(52, 2)) % 52];
+            }
+            if (size >= 3)
+            {
+                password[3] = chars[(i / (int)pow(52, 3)) % 52];
+            }
 
             hash = crypt(password, salt);
+            // printf("%s %s %s\n", password, argv[1], hash);
             if (strcmp(argv[1], hash) == 0)
+            // if (true)
             {
                 printf("%s %s %s\n", password, argv[1], hash);
                 return 0;
@@ -48,51 +50,82 @@ int main(int argc, char **argv)
         }
     }
 
-    for (int o = 0; o < 52; o++)
-    {
-        for (int p = 0; p < 52; p++)
-        {
-            for (int q = 0; q < 52; q++)
-            {
-                password[0] = chars[o];
-                password[1] = chars[p];
-                password[2] = chars[q];
-                password[3] = '\0';
+    // //testing for 1 chars words.
+    // for (int i = 0; i < 52; i++)
+    // {
+    //     password[0] = chars[i];
+    //     password[1] = '\0';
 
-                hash = crypt(password, salt);
-                if (strcmp(argv[1], hash) == 0)
-                {
-                    printf("%s %s %s\n", password, argv[1], hash);
-                    return 0;
-                }
-            }
-        }
-    }
+    //     hash = crypt(password, salt);
+    //     if (strcmp(argv[1], hash) == 0)
+    //     {
+    //         printf("%s %s %s\n", password, argv[1], hash);
+    //         return 0;
+    //     }
+    // }
 
-    for (int o = 0; o < 52; o++)
-    {
-        for (int p = 0; p < 52; p++)
-        {
-            for (int q = 0; q < 52; q++)
-            {
-                for (int r = 0; r < 52; r++)
-                {
-                    password[0] = chars[o];
-                    password[1] = chars[p];
-                    password[2] = chars[q];
-                    password[3] = chars[r];
-                    password[4] = '\0';
+    // for (int j = 0; j < 52; j++)
+    // {
+    //     for (int k = 0; k < 52; k++)
+    //     {
+    //         password[0] = chars[j];
+    //         password[1] = chars[k];
+    //         password[2] = '\0';
 
-                    hash = crypt(password, salt);
-                    if (strcmp(argv[1], hash) == 0)
-                    {
-                        printf("%s %s %s\n", password, argv[1], hash);
-                        return 0;
-                    }
-                }
-            }
-        }
-    }
+    //         hash = crypt(password, salt);
+    //         if (strcmp(argv[1], hash) == 0)
+    //         {
+    //             printf("%s %s %s\n", password, argv[1], hash);
+    //             return 0;
+    //         }
+    //     }
+    // }
+
+    // for (int o = 0; o < 52; o++)
+    // {
+    //     for (int p = 0; p < 52; p++)
+    //     {
+    //         for (int q = 0; q < 52; q++)
+    //         {
+    //             password[0] = chars[o];
+    //             password[1] = chars[p];
+    //             password[2] = chars[q];
+    //             password[3] = '\0';
+
+    //             hash = crypt(password, salt);
+    //             if (strcmp(argv[1], hash) == 0)
+    //             {
+    //                 printf("%s %s %s\n", password, argv[1], hash);
+    //                 return 0;
+    //             }
+    //         }
+    //     }
+    // }
+
+    // for (int o = 0; o < 52; o++)
+    // {
+    //     for (int p = 0; p < 52; p++)
+    //     {
+    //         for (int q = 0; q < 52; q++)
+    //         {
+    //             for (int r = 0; r < 52; r++)
+    //             {
+    //                 password[0] = chars[o];
+    //                 password[1] = chars[p];
+    //                 password[2] = chars[q];
+    //                 password[3] = chars[r];
+    //                 password[4] = '\0';
+
+    //                 hash = crypt(password, salt);
+    //                 if (strcmp(argv[1], hash) == 0)
+    //                 {
+    //                     printf("%s %s %s\n", password, argv[1], hash);
+    //                     return 0;
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
 }
 
 // int abc(char *s, int size)
