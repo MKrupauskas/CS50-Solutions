463fcc0f47065e49a4c103e6c221d312caf12be9
Mykolas Krupauskas <m.krupauskas@gmail.com>
2018-02-21 21:09:43 +0200

    add full race solution

--- chapter3/race.c
+++ chapter3/race.c
@@ -1,12 +1,13 @@
 //Helper functions for the sort race
 
 #include <cs50.h>
+#include <string.h>
 #include "helpers.h"
 
 //Returns true if str is a valid flag (-a, -b, -r, or -s), false otherwise
 bool check_flag(char *str)
 {
-    return false;
+    return (strcmp(str, "-a") == 0 || strcmp(str, "-b") == 0 || strcmp(str, "-r") == 0 || strcmp(str, "-s") == 0);
 }
 
 //Sorts array of n values using bubble sort.
@@ -30,17 +31,17 @@ void bubble(int values[], int n)
 //Sorts array of n values using selection sort.
 void selection(int values[], int n)
 {
-    int temp;
+    int temp, min;
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
+        min = i;
+        for (int j = i + 1; j < n; j++)
         {
-            if (values[i] > values[j]) {
-                temp = values[i];
-                values[i] = values[j];
-                values[j] = temp;
-            }
+            min = values[min] > values[j] ? j : min;
         }
+        temp = values[i];
+        values[i] = values[min];
+        values[min] = temp;
     }
     return;
 }
@@ -48,16 +49,16 @@ void selection(int values[], int n)
 //Sorts array of n values using insertion sort.
 void insertion(int values[], int n)
 {
-    int temp;
-    for (int i = 0; i < n; i++)
+    int temp, index;
+    for (int i = 1; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
+        index = i;
+        while (index > 0 && values[index] < values[index - 1])
         {
-            if (values[i] > values[j]) {
-                temp = values[i];
-                values[i] = values[j];
-                values[j] = temp;
-            }
+            temp = values[index];
+            values[index] = values[index - 1];
+            values[index - 1] = temp;
+            index--;
         }
     }
     return;
