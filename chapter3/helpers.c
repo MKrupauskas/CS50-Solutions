d2f86933f79887209879d90a813e3fe0dc402cf8
Mykolas Krupauskas <m.krupauskas@gmail.com>
2018-02-23 19:59:22 +0200

    add helpers.c

---
+++ chapter3/helpers.c
@@ -0,0 +1,34 @@
+/**
+ * helpers.c
+ *
+ * CS50 AP
+ * Find
+ *
+ * Helper functions.
+ */
+
+#include <cs50.h>
+
+#include "helpers.h"
+
+/**
+ * Returns true if value is in array of n values, else false.
+ */
+bool search(int value, int values[], int n)
+{
+    // TODO: implement a searching algorithm
+    if (n < 0)
+    {
+        return false;
+    }
+    return false;
+}
+
+/**
+ * Sorts array of n values.
+ */
+void sort(int values[], int n)
+{
+    // TODO: implement an O(n^2) sorting algorithm
+    return;
+}
